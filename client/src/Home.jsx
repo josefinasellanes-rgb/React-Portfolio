4b71c2b069f7eac59ae51ba500c653032bc7fb27
josefinasellanes-rgb <josefina.sellanes@gmail.com>
2025-12-04 21:07:53 -0300

    Added new paragraph for CI/CD demo

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -14,6 +14,7 @@ export default function Home({ user, projects, education = [] }) {
                 Welcome to the Home Page!
             </h1>
             {user && <p className="textHome"> I hope you enjoy the page, {user.username}!</p>}
+            <p className='textHome'> This is a new update for CI/CD demonstration</p>
             <p className='textHome'>I build elegant and accessible web experiences</p>
             <p className='textHome'>My mission: deliver clean, maintainable front-end code that delights users and makes product teams more productive.</p>
         </div>
